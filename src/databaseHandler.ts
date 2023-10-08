afe3ec9195a729e725e3e6634255b6cab1b26174
nandobfer <nandobfer@yahoo.com.br>
2023-10-08 13:14:14 -0300

    fix: new order query

--- src/databaseHandler.ts
+++ src/databaseHandler.ts
@@ -7,10 +7,24 @@ const inclusions = {
 }
 
 const order = {
-    new: async (data: Order, _shipping: Shipping, _billing: Billing) => {
-        const shipping = await prisma.shipping.create({ data: _shipping })
-        const billing = await prisma.billing.create({ data: _billing })
-        const order = await prisma.order.create({ data: { ...data, shipping_id: shipping.id, billing_id: billing.id }, include: inclusions.order })
+    new: async (data: Order & { shipping: Shipping; billing: Billing }) => {
+        const shipping = await prisma.shipping.create({ data: data.shipping })
+        const billing = await prisma.billing.create({ data: data.billing })
+        const order = await prisma.order.create({
+            data: {
+                id: data.id,
+                status: data.status,
+                customer_id: data.customer_id,
+                cart_hash: data.cart_hash,
+                date_created: data.date_created,
+                date_modified: data.date_modified,
+                order_key: data.order_key,
+                total: data.total,
+                shipping_id: shipping.id,
+                billing_id: billing.id,
+            },
+            include: inclusions.order,
+        })
 
         return order
     },
