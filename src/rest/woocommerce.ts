4d5c084bc898dc8dfa0386077cc91a101fb45fab
nandobfer <nandobfer@yahoo.com.br>
2023-10-08 23:24:04 -0300

    test: logging meta data

--- src/rest/woocommerce.ts
+++ src/rest/woocommerce.ts
@@ -11,7 +11,7 @@ router.post("/new_order", async (request: Request, response: Response) => {
         data.date_modified = data.date_modified.date
 
         const order = await databaseHandler.order.new(data)
-        console.log({ order, data })
+        console.log({ order, data, meta: data.meta_data })
     } catch (error) {
         console.log(error)
     }
